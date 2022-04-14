7896e21e7852916410a9845117cd0f88d2daa454
Wanyi <wanyilee0805@gmail.com>
2022-04-13 21:26:58 -0700

    make my listing page working

--- frontend/src/components/MyListing/index.js
+++ frontend/src/components/MyListing/index.js
@@ -1,11 +1,27 @@
 import './MyListing.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal } from '../../context/Modal';
 import  AddProductForm  from '../AddProductFormModal/AddProductForm';
+import { useDispatch, useSelector} from 'react-redux'
+import { loadProductsPage } from '../../store/products';
+import * as sessionActions from '../../store/session'
 
 function MyListingPage(){
+  const dispatch = useDispatch()
+  const sessionUser = useSelector(state => state.session.user)
+  const productsStateData = useSelector(state => state.productsState)
+  const products = Object.values(productsStateData);
+  const userProducts = products.filter(product => product.sellerId === sessionUser.id)
+
+  console.log("userProducts", userProducts)
 
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    dispatch(loadProductsPage())
+  }, [dispatch])
+
+
 
   return (
     <>
@@ -15,7 +31,16 @@ function MyListingPage(){
           <AddProductForm />
         </Modal>
       )}
-      
+      <div className='my-product-outter-container'>
+          {userProducts.map(userProduct => (
+            <div className='my-product-container'>
+              <div className='my-product-id'>{userProduct.id}</div>
+              <div className='my-product-title'>{userProduct.title}</div>
+              <div className='my-product-description'>Description: {userProduct.description}</div>
+            </div>
+          ))
+        }
+      </div>
     </>
   );
 }
