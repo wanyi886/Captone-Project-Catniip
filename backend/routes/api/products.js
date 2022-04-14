b999b836656411221b89618a4181fa5809a07e3f
Wanyi <wanyilee0805@gmail.com>
2022-04-14 10:04:43 -0700

    added form validation for creating and updating product

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -7,6 +7,28 @@ const { Product } = require('../../db/models')
 
 const router = express.Router();
 
+const validateProduct = [
+  check('title')
+    .exists({checkFalsy: true})
+    .withMessage('Title cannot be empty.'),
+  check('description')
+    .exists({checkFalsy: true})
+    .withMessage('Description cannot be empty.'),
+  check('imgUrl')
+    .exists({checkFalsy: true})
+    .withMessage('Image URL cannot be empty.'),
+  check('price')
+    .exists({checkFalsy: true})
+    .withMessage('Price cannot be empty.')
+    .isFloat({ min: 0 })
+    .withMessage('Price cannot be less than 0.'),
+  check('inventory')
+    .exists({checkFalsy: true})
+    .withMessage('Inventory cannot be empty')
+    .isInt({ min: 1 })
+    .withMessage('Inventory cannot be less than 1'),
+]
+
 
 // get all products
 router.get('/', asyncHandler(async (req, res) => {
@@ -25,9 +47,7 @@ router.get('/:id/detail', asyncHandler(async(req, res) => {
 
 // create a product
 
-// const validateC
-
-router.post('/', asyncHandler(async(req, res) => {
+router.post('/', validateProduct, handleValidationErrors, asyncHandler(async(req, res) => {
   const data = req.body;
   const newProduct = await Product.create(data);
 
@@ -35,7 +55,7 @@ router.post('/', asyncHandler(async(req, res) => {
 }))
 
 // update a product
-router.put('/:id', asyncHandler(async(req, res) => {
+router.put('/:id', validateProduct, handleValidationErrors, asyncHandler(async(req, res) => {
   const id = req.params.id;
   const targetProduct = await Product.findByPk(id);
   const updatedProduct = req.body;
