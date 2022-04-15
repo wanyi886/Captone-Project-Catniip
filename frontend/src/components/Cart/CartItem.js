7e9228cab28dfb3119ddc40ec874ab3b731724fd
Wanyi <wanyilee0805@gmail.com>
2022-04-15 11:20:47 -0700

    can update the count in the state, but setFinalCount not working

--- frontend/src/components/Cart/CartItem.js
+++ frontend/src/components/Cart/CartItem.js
@@ -1,6 +1,26 @@
 import { updateCount, removeFromCart } from '../../store/cart'
+import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react';
 
 function CartItem ({item}) {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart)
+  console.log("cart!!", cart)
+  const [finalCount, setFinalCount] = useState(cart[item.id].count)
+  console.log("finalCount", finalCount)
+
+  const handleAdd = async () => {
+    const count = item.count
+    await dispatch(updateCount(item.id, count + 1))
+    setFinalCount(count)
+  }
+
+  const handleSubstract = async () => {
+    const count = item.count
+    await dispatch(updateCount(item.id, count - 1))
+    setFinalCount(count)
+  }
+
   return (
     <div className='cart-item-container'>
       <div className='cart-item-image-container'>
@@ -15,13 +35,13 @@ function CartItem ({item}) {
       </div>
 
       <div className='cart-item-count-area'>
-        <span className='cart-item-count-decreate'>
+        <span className='cart-item-count-decreate' onClick={handleSubstract}>
           -
         </span>
         <span className='cart-item-count-number'>
           {item.count}
         </span>
-        <span className='cart-item-count-increase'>
+        <span className='cart-item-count-increase' onClick={handleAdd}>
           +
         </span>
       </div>
