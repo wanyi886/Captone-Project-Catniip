df5f28a39af06f8b7f50e571547203711b163197
Wanyi <wanyilee0805@gmail.com>
2022-04-17 15:33:42 -0700

    complete product detail page styling

--- frontend/src/components/ProductDetail/index.js
+++ frontend/src/components/ProductDetail/index.js
@@ -35,17 +35,29 @@ function ProductDetail () {
   }
 
   return (
-    <div className="product-detial-container">
-      <h1>Product Detail</h1>
-      <div className="img-container">
-        <img src={`${product?.imgUrl}`}/>
+    <div className="product-detail-page-body">
+      {/* <h1>Product Detail</h1> */}
+      <div className="product-detail-info-area">
+        <div className="product-img-container">
+          <img src={`${product?.imgUrl}`}/>
+        </div>
+        <div className="non-image-container">
+          {/* <div>Product Id: {product?.id}</div> */}
+          <div className="product-detail-title">{product?.title}</div>
+          <div className="product-detail-price">$ {product?.price}</div>
+          <div className="product-detail-des">{product?.description}</div>
+          <div className="product-detail-btn-container">
+            <button onClick={handleClick} className="product-detail-cart-btn">
+              <i class="fa-solid fa-cart-plus"></i>
+              Add to Cart
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="non-image-container">
-        <div>Product Id: {product?.id}</div>
-        <div>{product?.title}</div>
-        <div>{product?.price}</div>
-        <div>{product?.description}</div>
-        <button onClick={handleClick}>Add to Cart</button>
+
+      <div className="reviews-area">
+        <h1 className="review-h1">Reviews</h1>
+        <div className="no-review">No Reviews for this product now</div>
       </div>
     </div>
   )
