4d0a34734d007e1fe52a985942122ae1e6314eeb
Wanyi <wanyilee0805@gmail.com>
2022-04-20 15:47:13 -0700

    add second area for homepage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,5 +1,6 @@
 import './HomePage.css'
 import image1 from '../../images/homepage/pink-bg-cat.jpg'
+import image2 from '../../images/homepage/different-colors-eyes.jpg'
 import { Link, useHistory } from 'react-router-dom'
 
 function HomePage() {
@@ -15,12 +16,22 @@ function HomePage() {
       {/* <h1>HomePage</h1> */}
       <div className='home-page-first-go-shopping-area'>
         <div className='go-shopping-img-container'>
-          <img src={image1}></img>
+          <img src={image1} alt="cat-img-1"></img>
         </div>
         <div className='go-shopping-words-container'>
           <h1 className='home-page-go-shopping-h1'>Color Comes Alive!</h1>
           <h3 className='home-page-go-shopping-h3'>It’s time for a style refresh. Find your signature color with our Carriers, Toys and more, and secure a seasonal fit for you and your cat!</h3>
           <button onClick={handleShopping} className="go-shopping-btn">Go Shopping</button>
+        </div>
+      </div>
+
+      <div className="second-area">
+        <div className='second-area-words-container'>
+          <h1 className='second-area-h1'>Why Catnnip</h1>
+          <p className='second-area-words'>Catnup is the award-winning cat lifestyle brand for happy and healthy cats. Our innovative and delicate design and premium food recipe have won over 1 million cat parents' love.</p>
+        </div>
+        <div className='second-area-image-container'>
+          <img src={image2} alt="cat-img-2"/>
         </div>
 
       </div>
