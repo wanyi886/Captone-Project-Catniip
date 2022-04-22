fcf1553c136596a8336cceda27122c6994971c0d
Wanyi <wanyilee0805@gmail.com>
2022-04-22 16:02:59 -0700

    completed validation for creating and updating product

--- frontend/src/components/AddProductFormModal/AddProductForm.js
+++ frontend/src/components/AddProductFormModal/AddProductForm.js
@@ -40,13 +40,12 @@ function AddProductForm({ hideForm }) {
   useEffect(() => {
     const errors = [];
     if (!imgUrl) errors.push("Image URL cannot be empty.")
-    if (!validator.isURL(imgUrl)) errors.push("Please enter a valid URL.")
 
     const isImage = (url) => {
       return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
     }
 
-    if (!isImage(imgUrl)) errors.push("Please make sure the URL is an image.")
+    if (!isImage(imgUrl) || !validator.isURL(imgUrl)) errors.push("Please enter a valid image URL.")
     if (!title) errors.push("Title cannot be empty.")
     if (title.length > 32) errors.push("Title cannot be over 32 characters.")
     if (!description) errors.push("Description cannot be empty.")
--- frontend/src/components/EditProductFormModal/EditProductForm.js
+++ frontend/src/components/EditProductFormModal/EditProductForm.js
@@ -26,7 +26,12 @@ function EditProductForm({ product, hideForm }) {
   useEffect(() => {
     const errors = [];
     if (!imgUrl) errors.push("Image URL cannot be empty.")
-    if (!validator.isURL(imgUrl)) errors.push("Please enter a valid URL.")
+
+    const isImage = (url) => {
+      return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+    }
+
+    if (!isImage(imgUrl) || !validator.isURL(imgUrl)) errors.push("Please enter a valid image URL.")
     if (!title) errors.push("Title cannot be empty.")
     if (title.length > 32) errors.push("Title cannot be over 32 characters.")
     if (!description) errors.push("Description cannot be empty.")
