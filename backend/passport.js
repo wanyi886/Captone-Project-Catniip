c8e929d50e513c270e70d952defa460e1d5a625c
wanyi <wanyile0805@gmail.com>
2023-10-27 06:01:48 -0700

    deleted user import code in passport.js

--- backend/passport.js
+++ backend/passport.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GithubStrategy = require('passport-github2').Strategy;
-const { User } = require('../backend/db/');
 
 
 require('https').globalAgent.options.rejectUnauthorized = false;
