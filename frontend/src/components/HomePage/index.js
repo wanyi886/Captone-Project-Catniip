956d5e777d25ca036cd49723634fe2ff6f3290a5
wanyi <wanyile0805@gmail.com>
2023-12-28 19:57:13 -0800

    hide the location part in home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -36,7 +36,7 @@ function HomePage() {
 
       </div>
 
-      <div className="second-area">
+      {/* <div className="second-area">
         <div className='second-area-words-container'>
           <h1 className='second-area-h1'>Where are we</h1>
           <p className='second-area-words'>Catniip is a One-Stop-Shop for happy and healthy cats. As Cat Lovers, you can buy delecate products from food to carrier. If you have products that could level up cat's life, list it here! </p>
@@ -45,7 +45,7 @@ function HomePage() {
           <img src={image2} alt="cat-img-2"/>
         </div>
 
-      </div>
+      </div> */}
 
     </div>
   )
